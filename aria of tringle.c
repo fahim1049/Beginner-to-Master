d4a428d6a168b4ee87503f45e74d4597ada745ce
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-03 21:07:49 +0600

    Aria of Triangle

---
+++ aria of tringle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    float base,height,aria;
+
+        printf("Enter the base:");
+        scanf("%f",&base);
+
+            printf("Enter the height :");
+            scanf("%f"&height);
+
+                    aria = 0.5*base*height;
+
+                        printf("Aria of tringle:%.2f",aria);
+
+
+                     return 0;   
+}
