98839ad40d1eee7ac368805a8e2f277a45619fe6
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-04 16:14:35 +0600

    Power Equation

---
+++ x to the power y.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter the power of x :");
+    scanf("%d",&x);
+
+    printf("Enter the power of y :");
+    scanf("%d",&y);
+
+    double  p = pow(x,y);
+
+    printf(" Result is :%.2lf",p);
+
+    return 0;
+
+
+}
