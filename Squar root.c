ea397609c17446d7b689e3a9e9ce2df254e3689c
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-04 16:01:34 +0600

    Squar root

---
+++ Squar root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   int r;
+    printf("Enter any number :");
+    scanf("%d",&r);
+
+    double a =sqrt(r);
+
+    printf("Squartroot is :%.2lf\n",a);
+
+    return 0;
+}
