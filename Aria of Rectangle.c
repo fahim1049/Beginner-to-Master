54c741d836eb243a69662e5a5c379c33e80434d5
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-04 08:38:32 +0600

    Aria of Rectangle

---
+++ Aria of Rectangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+    int main()
+    {
+            float length,width,aria;
+
+            printf("Enter your length : ");
+            scanf("%f",&length);
+
+                printf("Enter your Width :");
+                scanf("%f",&width);
+
+                    aria = length*width;
+
+
+                        printf("Aria of Rectangle : %.2f",aria);
+
+
+
+                  return 0;
+
+
+    }
