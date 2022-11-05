723e1f2d2e37ab0b5eecfdc2d4458f15b0cadf70
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-05 14:32:40 +0600

    Find Even or Odd

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any number :");
+    scanf("%d",&num);
+
+    if(num%2==0)
+    {
+    printf("Even");
+    }
+    else
+{
+    printf("Odd");
+
+}
+    return 0;
+}
