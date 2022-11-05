4e8fa4b2a85b363fe29ca0998be4ab89ed65c8db
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-05 20:16:35 +0600

    possitive or negative

---
+++ p or n.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter number :");
+    scanf("%d",&num);
+
+    if(num>0)
+    Printf("Possitive");
+
+    else if
+    printf("Negative");
+
+    return 0;
+}
