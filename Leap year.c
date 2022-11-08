fdfbb0400893d72e7f32352f23e96351a227a4bb
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-11-08 18:34:43 +0600

    Find leap year

---
+++ Leap year.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+        printf("Enter year :");
+        scanf("%d",&year);
+
+        if(year%400==0)
+
+            printf("Leap Year");
+
+            else if(year%4==0 && year%100!=0)
+
+                printf("Leap year");
+
+                else
+                    printf(" Not leap year");
+
+            return 0;
+}
